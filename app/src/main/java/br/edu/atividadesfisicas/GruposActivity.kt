55977eba2edef8032ec3445be50aad24a7c2c169
Bruno Ruaro <brunoruaro@alunos.utfpr.edu.br>
2025-07-01 23:25:35 -0300

    tela de grupos e ajustes visuais

--- app/src/main/java/br/edu/atividadesfisicas/GruposActivity.kt
+++ app/src/main/java/br/edu/atividadesfisicas/GruposActivity.kt
@@ -1,51 +1,21 @@
 package br.edu.atividadesfisicas
 
-import android.content.Intent
 import android.os.Bundle
-import android.view.View
-import android.widget.Button
 import android.widget.ImageView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
-class MainActivity : AppCompatActivity() {
-
-    private lateinit var btnMonitor : Button;
-    private lateinit var btnRanking: Button
-
+class GruposActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
-        btnRanking = findViewById(R.id.btnRanking)
-        btnRanking.setOnClickListener {
-            verRanking()
-        }
+        setContentView(R.layout.activity_grupos)
 
         val ivQuestion = findViewById<ImageView>(R.id.ivQuestion)
         ivQuestion.setOnClickListener {
             mostrarInfoPontuacao()
         }
-    }
-    fun verRanking() {
-        val intent = Intent(this, RankingActivity ::class.java)
-        startActivity(intent)
-
-        btnMonitor = findViewById(R.id.btnMonitor);
-
-        btnMonitor.setOnClickListener(object : View.OnClickListener {
-            override fun onClick(v: View?) {
-                val intent = Intent(this@MainActivity, MonitorActivity::class.java)
-                startActivity(intent)
-            }
-        });
-    }
-
-
-    fun verGrupos(view: View) {
-        val intent = Intent(this, GruposActivity::class.java)
-        startActivity(intent)
     }
 
     private fun mostrarInfoPontuacao() {
@@ -63,4 +33,3 @@ class MainActivity : AppCompatActivity() {
         builder.show()
     }
 }
-
